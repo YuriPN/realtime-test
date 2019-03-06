c5ade3a1e5ab864753a4edb0d4e2dab6fc6bc018
YuriPN <ypedroso8@gmail.com>
2019-03-06 18:29:50 -0400

    Updates

---
+++ precache-manifest.4edcbec3246ee467c18aea4be0050a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b534336d1de88e76ef2",
+    "url": "/realtime-test/static/js/runtime~main.2b534336.js"
+  },
+  {
+    "revision": "2bb7c0f6089fe7027023",
+    "url": "/realtime-test/static/js/main.2bb7c0f6.chunk.js"
+  },
+  {
+    "revision": "baa24b9fba97a4973faf",
+    "url": "/realtime-test/static/js/2.baa24b9f.chunk.js"
+  },
+  {
+    "revision": "2bb7c0f6089fe7027023",
+    "url": "/realtime-test/static/css/main.e58ed012.chunk.css"
+  },
+  {
+    "revision": "cf70fe9236367074f5dd55e1d2347012",
+    "url": "/realtime-test/index.html"
+  }
+];
