db4b7e40c4a9a6cc9424f9363605a7b02e9c0232
AlanLucasSC <AlanLucasSC2011@gmail.com>
2019-03-08 18:13:52 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/realtime-test/precache-manifest.5c82a0ed1207820af36a2d1720f700a9.js"
+  "/realtime-test/precache-manifest.4af7387c3d76164019b9be4b85110aa5.js"
 );
 
 workbox.clientsClaim();
