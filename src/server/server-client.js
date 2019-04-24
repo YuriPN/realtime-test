51200f4d18bfcace83828df0f38e92a9a88a8701
YuriPN <ypedroso8@gmail.com>
2019-04-24 09:57:14 -0400

    herokuapp com otimização de tempo na resposta

--- src/server/server-client.js
+++ src/server/server-client.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
-//var endpoint = 'https://az-realtime-editor.herokuapp.com/';
-var endpoint = 'http://172.19.16.126:5000/';
+var endpoint = 'https://az-realtime-editor.herokuapp.com/';
+//var endpoint = 'http://172.19.16.126:5000/';
 const socket = io(endpoint);
 
 export default socket;
